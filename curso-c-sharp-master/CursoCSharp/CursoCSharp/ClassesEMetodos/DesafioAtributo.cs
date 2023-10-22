4b85b250081a47a619ebcafe05f20783f906b48e
luis henrique ramos soares <luishh205@gmail.com>
2023-10-22 13:25:40 -0300

    Sua mensagem de commit aqui

---
+++ curso-c-sharp-master/CursoCSharp/CursoCSharp/ClassesEMetodos/DesafioAtributo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class DesafioAtributo
+    {
+        int a = 10;
+        int b = 10;
+
+        public static void Executar() {
+            // Acessar variável 'a' dentro do método Executar!
+            // Console.WriteLine(a);
+
+            
+
+            DesafioAtributo desafio = new DesafioAtributo();
+            Console.WriteLine(desafio.a);
+        }
+    }
+}
+
+
